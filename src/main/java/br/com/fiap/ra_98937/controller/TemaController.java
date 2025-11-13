e260276a529e5d38d2687f774a9bc294ba4847bd
Guilherme Costa Proença <guilherme.proenca134@gmail.com>
2025-11-13 17:06:30 -0300

    refactor: Correct package naming and reintroduce Gs0talentplatformApplication class

--- src/main/java/br/com/fiap/ra_98937/controller/TemaController.java
+++ src/main/java/br/com/fiap/ra_98937/controller/TemaController.java
@@ -1,12 +1,14 @@
-package br.com.fiap.ra98937.controller;
+package br.com.fiap.ra_98937.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@Tag(name = "Tema", description = "Endpoint da Global Solution 2 - Microservice and Web Engineering")
 public class TemaController {
 
     @GetMapping("/info")
